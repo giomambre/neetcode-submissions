66c1407a9ca7ba8ca24091829ce64903375f2f52
Giovanni Mambretti <71758759+giomambre@users.noreply.github.com>
2026-09-25 21:22:01 +0200

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+
+            cur_len = len(s)
+            encoded +=  str(cur_len) +  "&" + s
+        return encoded
+    def decode(self, s: str) -> List[str]:
+        
+        decoded = []
+        i = 0
+        while i < len(s):
+            cur_len = ""
+            while s[i] != "&":
+                cur_len += s[i]
+                i+=1
+            i+=1
+            decoded.append(s[i:i+int(cur_len)])            
+            i+= int(cur_len)
+
+        return decoded
+            
